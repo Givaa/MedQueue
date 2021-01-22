302db0f1bf8674f92cbe51338eb74e6ccafaa5bb
angeloafeltra <73121300+angeloafeltra@users.noreply.github.com>
2021-01-22 13:23:09 +0100

    Popolamento parziale db

--- MedQueue.sql
+++ MedQueue.sql
@@ -58,3 +58,23 @@ Nome varchar(255) not null,
 Id_Struttura integer not null,
 primary key(Id),
 foreign key(Id_Struttura) references Struttura(Id) ON UPDATE CASCADE ON DELETE CASCADE );
+
+
+INSERT INTO Struttura(nome,indirizzo,Numero_di_telefono) VALUES
+('santobono','Via della Croce Rossa n. 8 Napoli (NA)','0812205111'),
+('San Leonardo','Viale Europa n.8 Castellammare di Stabia NA','081872911');
+
+INSERT INTO Impiegato VALUES
+('FLTNGL99A14L845J','angelo99','angelo','afeltra','1999/01/14','a.afeltra12@studenti.unisa.it','3394487295',1),
+('FCLNDR99C12B963C','andrea99','andrea','fucile','1999/03/12','andrea.fucile@studenti.unisa.it','3394487394',1),
+('RPAGNN95D11A509B','giovannirapa95','giovanni','rapa','1995/01/16','g.rapa95@gmail.com','3394487293',2),
+('ADRAMT99D13A587J','amato99','adriano','amato','1999/07/23','amatoadriano@gmail.com','3457892239',2);
+
+
+INSERT INTO Operazione(Tipo_Operazione,Descrizione) VALUES
+('Pagamento Ticket','Pagamento Ticket per visita medica'),
+('Prenotazione Ambulatorio','Richiesta prenotazione ambulatorio');
+
+
+
+
