64ebc501f93fcf5e6ed157ba98d53c65fff957a9
angeloafeltra <73121300+angeloafeltra@users.noreply.github.com>
2021-01-22 18:50:36 +0100

    Aggiornamento dataAcces

--- AppDesktopMedQueue/src/business/PrenotazioneBean.java
+++ AppDesktopMedQueue/src/business/PrenotazioneBean.java
@@ -1,9 +1,41 @@
 package business;
 
+
 public class PrenotazioneBean {
     private int id;
     private String data;
-    private
+    private String time;
+    private boolean convalida;
+    private String codicefiscale;
+    private int id_operazione;
+    private int  id_struttura;
 
+    public PrenotazioneBean(){}
+    public PrenotazioneBean(int id, String data, String time, boolean convalida, String codicefiscale, int id_operazione, int id_struttura) {
+        this.id = id;
+        this.data = data;
+        this.time = time;
+        this.convalida = convalida;
+        this.codicefiscale = codicefiscale;
+        this.id_operazione = id_operazione;
+        this.id_struttura = id_struttura;
+    }
 
+    //GET
+    public int getId() { return id; }
+    public String getData() { return data; }
+    public String getTime() { return time; }
+    public boolean isConvalida() { return convalida; }
+    public String getCodicefiscale() { return codicefiscale; }
+    public int getId_operazione() { return id_operazione; }
+    public int getId_struttura() { return id_struttura; }
+
+    //SET
+    public void setId(int id) { this.id = id; }
+    public void setData(String data) { this.data = data; }
+    public void setTime(String time) { this.time = time; }
+    public void setConvalida(boolean convalida) { this.convalida = convalida; }
+    public void setCodicefiscale(String codicefiscale) { this.codicefiscale = codicefiscale; }
+    public void setId_operazione(int id_operazione) { this.id_operazione = id_operazione; }
+    public void setId_struttura(int id_struttura) { this.id_struttura = id_struttura; }
 }
--- AppDesktopMedQueue/src/business/StrutturaBean.java
+++ AppDesktopMedQueue/src/business/StrutturaBean.java
@@ -1,4 +1,30 @@
 package business;
 
 public class StrutturaBean {
+
+    private int id;
+    private String nome;
+    private String indirizzo;
+    private String numerotelefono;
+
+    public StrutturaBean(){}
+    public StrutturaBean(int id, String nome, String indirizzo, String numerotelefono) {
+        this.id = id;
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.numerotelefono = numerotelefono;
+    }
+
+    //GET
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getIndirizzo() { return indirizzo; }
+    public String getNumerotelefono() { return numerotelefono; }
+
+    //SET
+    public void setId(int id) { this.id = id; }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo; }
+    public void setNumerotelefono(String numerotelefono) { this.numerotelefono = numerotelefono; }
 }
+
