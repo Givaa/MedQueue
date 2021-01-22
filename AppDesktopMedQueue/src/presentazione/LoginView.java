487f854cc57c084d62615d0330483ba96d6f906e
angeloafeltra <73121300+angeloafeltra@users.noreply.github.com>
2021-01-22 12:57:04 +0100

    appDesktop

--- AppDesktopMedQueue/src/presentazione/LoginView.java
+++ AppDesktopMedQueue/src/presentazione/LoginView.java
@@ -5,7 +5,7 @@ import persistence.DataAccess;
 import javax.swing.*;
 import java.awt.*;
 
-public class ProgrammaView extends JFrame{
+public class LoginView extends JFrame{
 
     private static final long serialVersionUID = 1L;
     private static JFrame framePannello = new JFrame();
@@ -14,35 +14,45 @@ public class ProgrammaView extends JFrame{
 
     public static void main(String[] args) {
 
+
         JTextField TF_CF = new JTextField(16);
         JTextField TF_pass = new JTextField(32);
         JButton connect = new JButton("Connetti");
+        JLabel errore=new JLabel();
+        errore.setForeground(Color.red); //Setto il colore del testo a rosso
+        errore.setHorizontalAlignment(JLabel.CENTER); //Centro il testo nella JLabel
+        JLabel insCF= new JLabel("Inserire codice fiscale:");
+        JLabel insPass= new JLabel(" Inserire password");
+
         connect.addActionListener(l -> {
             if(DataAccess.connect()) {
-                if(DataAccess.verificaDatiImpiegato(TF_CF.getText(),TF_pass.getText())==false)
-                    JOptionPane.showMessageDialog(null, "Credenziali errate");
+                if(DataAccess.verificaDatiImpiegato(TF_CF.getText(),TF_pass.getText())==true) {
+                    new SelectQueueView().visible(true);
+                    framePannello.setVisible(false);
+                }
+                else
+                    errore.setText("Credenziali errate");
             } else {
-                JOptionPane.showMessageDialog(null, "Connessione non riuscita.");
+               errore.setText("Errore nella connessione");
             }
         });
 
 
         ImageIcon immagine = new ImageIcon("src/image/LogoNoBG.png");
         Image image = immagine.getImage();
-        Image newimg = image.getScaledInstance(250, 200,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        Image newimg = image.getScaledInstance(250, 180,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
         immagine = new ImageIcon(newimg);
         JLabel contenitoreImmagine = new JLabel(immagine, JLabel.CENTER);
 
         JPanel pannello = new JPanel();
         pannello.setLayout(new GridLayout(6,1));
+        pannello.setBackground( Color.white );
         pannello.setBorder(BorderFactory.createTitledBorder("Accesso"));
-        JLabel insCF= new JLabel("Inserire codice fiscale:");
-        JLabel insPass= new JLabel(" Inserire password");
         pannello.add(insCF);
         pannello.add(TF_CF);
         pannello.add(insPass);
         pannello.add(TF_pass);
-        pannello.add(new JLabel());
+        pannello.add(errore);
         pannello.add(connect);
 
         framePannello.add(pannello, BorderLayout.CENTER);
@@ -52,6 +62,7 @@ public class ProgrammaView extends JFrame{
         framePannello.setVisible(true);
         framePannello.setLocationRelativeTo(null);
         framePannello.setSize(400, 400);
+        framePannello.getContentPane().setBackground( Color.white );
         framePannello.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
