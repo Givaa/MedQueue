71a44876a8096e24338601a71d57fb4dec4614e4
Giovanni Rapa <48215666+Givaa@users.noreply.github.com>
2021-01-22 21:57:07 +0100

    changes to connectionPool

--- AppDesktopMedQueue/src/persistence/DriverManagerConnectionPool.java
+++ AppDesktopMedQueue/src/persistence/DriverManagerConnectionPool.java
@@ -8,62 +8,62 @@ import java.util.List;
 
 public class DriverManagerConnectionPool {
 
-        private static List<Connection> freeDbConnections;
+    private static List<Connection> freeDbConnections;
 
-        static {
-            freeDbConnections = new LinkedList<Connection>();
+    static {
+        reeDbConnections = new LinkedList<Connection>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("DB driver not found:"+ e.getMessage());
+        }
+    }
+
+    public DriverManagerConnectionPool() {
+        freeDbConnections = new LinkedList<Connection>();
+    }
+
+    public synchronized static Connection createDBConnection() {
+        try {
+            Connection newConnection = null;
+            String ip = "localhost";
+            String port = "3306";
+            String db = "medqueue?serverTimezone=UTC&useLegacyDatetimeCode=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false";
+            String username = "root";
+            String password = "root";
+
+            newConnection = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + db, username, password);
+
+            newConnection.setAutoCommit(true);
+            return newConnection;
+        } catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static synchronized Connection getConnection() throws SQLException {
+        Connection connection;
+
+        if (!freeDbConnections.isEmpty()) {
+            connection = (Connection) freeDbConnections.get(0);
+            freeDbConnections.remove(0);
+
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-            } catch (ClassNotFoundException e) {
-                System.out.println("DB driver not found:"+ e.getMessage());
-            }
-        }
-
-        public DriverManagerConnectionPool() {
-            freeDbConnections = new LinkedList<Connection>();
-        }
-
-        public synchronized static Connection createDBConnection() {
-            try {
-                Connection newConnection = null;
-                String ip = "localhost";
-                String port = "3306";
-                String db = "medqueue?serverTimezone=UTC&useLegacyDatetimeCode=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false";
-                String username = "root";
-                String password = "root";
-
-                newConnection = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + db, username, password);
-
-                newConnection.setAutoCommit(true);
-                return newConnection;
-            } catch(Exception e){
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        public static synchronized Connection getConnection() throws SQLException {
-            Connection connection;
-
-            if (!freeDbConnections.isEmpty()) {
-                connection = (Connection) freeDbConnections.get(0);
-                freeDbConnections.remove(0);
-
-                try {
-                    if (connection.isClosed())
-                        connection = getConnection();
-                } catch (SQLException e) {
-                    connection.close();
+                if (connection.isClosed())
                     connection = getConnection();
-                }
-            } else {
-                connection = createDBConnection();
+            } catch (SQLException e) {
+                connection.close();
+                connection = getConnection();
             }
-
-            return connection;
+        } else {
+            connection = createDBConnection();
         }
+		
+        return connection;
+    }
 
-        public synchronized static void releaseConnection(Connection connection) throws SQLException {
-            if(connection != null) freeDbConnections.add(connection);
-        }
+    public synchronized static void releaseConnection(Connection connection) throws SQLException {
+        if(connection != null) freeDbConnections.add(connection);
+    }
 }
