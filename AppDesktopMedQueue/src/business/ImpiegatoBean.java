82b4531fe7b619e0c71449e3a2973f3640343154
angeloafeltra <73121300+angeloafeltra@users.noreply.github.com>
2021-01-25 20:26:35 +0100

    javadoc

--- AppDesktopMedQueue/src/business/ImpiegatoBean.java
+++ AppDesktopMedQueue/src/business/ImpiegatoBean.java
@@ -1,6 +1,10 @@
 package business;
 
+/**
+ * Classe che rappresenta l'impiegato ospedaliero
+ */
 public class ImpiegatoBean {
+
 
   private String codicefiscale;
   private String password;
@@ -11,8 +15,22 @@ public class ImpiegatoBean {
   private String numeroDiTelefono;
   private int idStruttura;
 
+  /**
+   * Inizzializza un nuovo oggetto ImpiegatoBean
+   */
   public ImpiegatoBean() {}
 
+  /**
+   * Inizzialissa un nuovo oggetto ImpiegatoBean settando gli attributi
+   * @param codicefiscale codicefiscale dell'impiegato
+   * @param password password d'accesso al sistema dell'impiegato
+   * @param nome nome impiegato
+   * @param cognome cognome impiegato
+   * @param dataDiNascita data di nascita dell'impiegato
+   * @param indirizzoEmail indirizzo email dell'impiegato
+   * @param numeroDiTelefono numero di telefono dell'impiegato
+   * @param idStruttura id della struttura per cui lavaora l'impiegato
+   */
   public ImpiegatoBean(
       String codicefiscale,
       String password,
@@ -32,68 +50,131 @@ public class ImpiegatoBean {
     this.idStruttura = idStruttura;
   }
 
-  // GET
+
+  /**
+   * Metodo che restituisce il codice fiscale dell'impigato
+   * @return codicefiscale
+   */
   public String getCodicefiscale() {
     return codicefiscale;
   }
 
-  // SET
+  /**
+   * Metodo per settare un codice fiscale all'oggetto ImpiegatoBean
+   * @param codicefiscale codicefiscale dell'impiegato
+   */
   public void setCodicefiscale(String codicefiscale) {
     this.codicefiscale = codicefiscale;
   }
 
+  /**
+   * Metodo che restituisce la password dell'impiegato
+   * @return password
+   */
   public String getPassword() {
     return password;
   }
 
+  /**
+   * Metodo per settare la password all'oggetto ImpiegatoBean
+   * @param password password dell'impiegato
+   */
   public void setPassword(String password) {
     this.password = password;
   }
 
+  /**
+   * Metodo per ottenere il nome del impiegato
+   * @return nome
+   */
   public String getNome() {
     return nome;
   }
 
+  /**
+   * Metodo per settare il nome all'oggetto ImpiegatoBean
+   * @param nome nome dell'impiegato
+   */
   public void setNome(String nome) {
     this.nome = nome;
   }
 
+  /**
+   * Netodo che ritorna il cognome dell'impiegato
+   * @return cognome
+   */
   public String getCognome() {
     return cognome;
   }
 
+  /**
+   * Metodo per settare il cognome all'oggetto ImpiegatoBean
+   * @param cognome cognome dell'impiegato
+   */
   public void setCognome(String cognome) {
     this.cognome = cognome;
   }
 
+  /**
+   * Metodo per ottenere la data di nascita dell'impiegato
+   * @return data di nascita
+   */
   public String getDataDiNascita() {
     return dataDiNascita;
   }
 
+  /**
+   * Metodo per settare la data di nascita all'oggetto ImpiegatoBean
+   * @param dataDiNascita data di nascita dell'impiegato
+   */
   public void setDataDiNascita(String dataDiNascita) {
     this.dataDiNascita = dataDiNascita;
   }
 
+  /**
+   * Metodo per ottenere l'indirizzo email dell'impiegato
+   * @return indirizzo email
+   */
   public String getIndirizzoEmail() {
     return indirizzoEmail;
   }
 
+  /**
+   * Metodo per settare l'indirizzo email all'oggetto ImpiegatoBean
+   * @param indirizzoEmail indirizzo email dell'impiegato
+   */
   public void setIndirizzoEmail(String indirizzoEmail) {
     this.indirizzoEmail = indirizzoEmail;
   }
 
+  /**
+   * Metodo per ottenere il numero di telefono dell'impiegato
+   * @return numero di telefono
+   */
   public String getNumeroDiTelefono() {
     return numeroDiTelefono;
   }
 
+  /**
+   * Metdodo per settare il numero di telefono all'oggetto ImpiegatoBean
+   * @param numeroDiTelefono numero di telefondo dell'impiegato
+   */
   public void setNumeroDiTelefono(String numeroDiTelefono) {
     this.numeroDiTelefono = numeroDiTelefono;
   }
 
+  /**
+   * Metodo per ottenere l'id della struttura dove lavora l'impiegato
+   * @return id della struttura ospedaliera
+   */
   public int getIdStruttura() {
     return idStruttura;
   }
 
+  /**
+   * Metodo per settare l'id della struttura ospedaliera all'oggetto ImpiegatoBean
+   * @param idStruttura id struttura ospedaliera dell'impeigato
+   */
   public void setIdStruttura(int idStruttura) {
     this.idStruttura = idStruttura;
   }
