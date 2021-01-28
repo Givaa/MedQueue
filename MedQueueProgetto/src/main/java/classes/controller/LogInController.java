71abfa24de7ca816ae690055b5f363391f3d3439
Demux23 <68477271+Demux23@users.noreply.github.com>
2021-01-28 13:39:15 +0100

    Aggiunto signup e cambiato il javadoc

--- MedQueueProgetto/src/main/java/classes/controller/LogInController.java
+++ MedQueueProgetto/src/main/java/classes/controller/LogInController.java
@@ -2,6 +2,7 @@ package classes.controller;
 
 import classes.model.bean.entity.UtenteBean;
 import classes.model.dao.UtenteModel;
+import java.sql.Date;
 import java.sql.SQLException;
 import javax.servlet.http.HttpServlet;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +24,35 @@ public class LogInController extends HttpServlet {
    * @throws SQLException per problemi di esecuzione della query
    */
   @GetMapping("/login")
-  public boolean login(@RequestBody String username, @RequestBody String password) throws SQLException {
+  public boolean login(@RequestBody String username,
+                       @RequestBody String password) throws SQLException {
     UtenteBean a = um.doRetrieveByKey(username);
 
     return a.getPassword().equals(password);
+  }
+
+  /**
+   * Metodo che permette di registrarsi al sito.
+   *
+   * @param codFisc Chiave primaria dell'utente
+   * @param password Password dell'utente
+   * @param nome Nome dell'utente
+   * @param cognome cognome dell'utente
+   * @param dataDiNascita data di nascita dell'utente
+   * @param email email dell'utente
+   * @param numeroDiTelefono numero di telefono dell'utente
+   * @return conferma o meno della registrazione
+   * @throws SQLException per problemi di esecuzione della query
+   */
+  @GetMapping("/signup")
+  public boolean signup(@RequestBody String codFisc, @RequestBody String password,
+                        @RequestBody String nome, @RequestBody String cognome,
+                        @RequestBody Date dataDiNascita, @RequestBody String email,
+                        @RequestBody int numeroDiTelefono) throws SQLException {
+    UtenteBean a = new UtenteBean(codFisc, password, nome, cognome,
+            dataDiNascita, email, numeroDiTelefono);
+    um.doSave(a);
+
+    return true;
   }
 }
