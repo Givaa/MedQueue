f22419d960b30351bd781adca0d52b62c1345ef5
angeloafeltra <angelo.afeltra99@gmail.com>
2021-02-05 19:18:54 +0100

    Revert "Merge branch 'main' of https://github.com/Demux23/FRAA into main"
    This reverts commit f5e08c782867c30b17b84c1a76033a6cafd64d03, reversing
    changes made to 49a3241c4e97a1877ea8e1127a797bddbf20e1bb.

--- MedQueueInterface/src/views/Login.vue
+++ MedQueueInterface/src/views/Login.vue
@@ -70,7 +70,9 @@ export default {
     async onSubmit(){
       loginAxios.login(this.loginForm.username, this.loginForm.password)
       .then((response) => {
+        console.log(response);
         if(response === ''){
+          console.log("Errore");
           this.presentAlert();
           return null;
         }else {
